92a595e0d04112948bbf86df1cd52d30e9132019
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2026-03-17 17:24:44 +0700

    test: enhance UsersService findOne method with NotFoundException handling and detailed assertions

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { MinioService } from '../minio/minio.service';
+import { NotFoundException } from '@nestjs/common';
+import { min } from 'class-validator';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -57,7 +59,52 @@ describe('UsersService', () => {
     minioService = module.get<MinioService>(MinioService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    const userId = 'user-id';
+
+    it('happy path: should return user data with enriched avatar URL', async () => {
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(userData);
+      (minioService.getPresignedUrl as jest.Mock).mockResolvedValueOnce(
+        'url/avatar.jpg',
+      );
+
+      const user = await service.findOne(userId);
+
+      expect(user).toBe(userData);
+      expect(user).toEqual(userData);
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { id: userId },
+        select: {
+          id: true,
+          email: true,
+          fullName: true,
+          avatar: true,
+          usedStorage: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+      expect(minioService.getPresignedUrl).toHaveBeenCalledWith(
+        userData.avatar,
+      );
+    });
+
+    it('error path: should throw NotFoundException if user is not found', async () => {
+      (prismaService.user.findUnique as jest.Mock).mockResolvedValue(null);
+
+      await expect(service.findOne('not-found-id')).rejects.toThrow(
+        NotFoundException,
+      );
+
+      expect(minioService.getPresignedUrl).not.toHaveBeenCalled();
+    });
   });
 });
